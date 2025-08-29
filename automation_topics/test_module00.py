0f1f6c46a33dac641c5e098f8d07e8d15778672c
Ruchet <ruchetkokare@gmail.com>
2025-08-29 15:49:06 +0530

    initial commit

---
+++ automation_topics/test_module00.py
@@ -0,0 +1,11 @@
+class TestMyStuff():
+   def test_type(self):
+       assert type(1.5) == int
+
+   def test_str(self):
+       assert str.upper("Anil")=="ANIl"
+class TestMyStuffs():
+   def test_type33(self):
+       assert type(1.5) == float
+
+
